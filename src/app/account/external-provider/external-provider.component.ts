998ccbcbbd47892fffc098dd99389164bd76327e
Rigo <rigoxls@gmail.com>
2020-04-19 11:07:11 -0500

    api panel e provider

--- src/app/account/external-provider/external-provider.component.ts
+++ src/app/account/external-provider/external-provider.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit, OnDestroy} from '@angular/core';
 import {ExternalProviderService} from './shared/external-provider.service';
 import {Subscription} from 'rxjs';
 import {AuthService} from '../shared/auth.service';
+import beautify from 'xml-beautifier';
 
 import {User} from '../../models/user.model';
 import {Router} from '@angular/router';
@@ -15,6 +16,9 @@ export class ExternalProviderComponent implements OnInit, OnDestroy {
   private authSubscription: Subscription;
   private user: User;
   private requests: any;
+  public connectionPubSub = null;
+  public payloadJson = null;
+  public payloadXml = null;
 
   constructor(
     public providerService: ExternalProviderService,
@@ -23,8 +27,37 @@ export class ExternalProviderComponent implements OnInit, OnDestroy {
   ) {
     this.user = JSON.parse(atob(localStorage.getItem('user')));
     if (this.user) {
-
     }
+
+    this.connectionPubSub = `
+      async function rabbitMQBootstrap() {
+          const app = await NestFactory.createMicroservice(AppModule, {
+          strategy: new RabbitMQServer(
+            'amqp://kxsfcakg:jahgl20UG_mvcXK9f7keJpazfDSJtarc@prawn.rmq.cloudamqp.com/kxsfcakg'
+          ),
+        });
+        await app.listen(() => {});
+    }`;
+
+    this.payloadJson = `
+        {
+          "id": 1,
+          "catalogId": 1,
+          "price": 50000,
+          "name": "Product 1",
+          "description": "Product 1 description",
+          "imageUrl": "https://www.psdmockups.com/wp-content/uploads/2017/08/Square-Corrugated-Cardboard-Shipping-Box-PSD-Mockup-520x392.jpg",
+          "createDate": 1586369231251
+        }`;
+
+    this.payloadXml = beautify(`
+      <?xml version="1.0" encoding="UTF-8"?>
+      <note>
+        <to>Tove</to>
+        <from>Jani</from>
+        <heading>Reminder</heading>
+        <body>Don't forget me this weekend!</body>
+      </note>`);
   }
 
   ngOnInit() {
